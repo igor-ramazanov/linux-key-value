53f5e7892eeeeff9b7de88d19af7a46959b2b31c
Igor Ramazanov <themirrortruth@gmail.com>
2017-09-24 00:00:17 +0200

    Small reformatting

--- module/command.c
+++ module/command.c
@@ -1,48 +1,46 @@
-#include <linux/string.h>
 #include <linux/slab.h>
 
-struct Command {
-    //0 - Set, 1 - Get
-    char operation;
-    int key_size;
-    int value_size;
-    char *key;
-    char *value;
+struct command {
+  //0 - Set, 1 - Get
+  char operation;
+  int key_size;
+  int value_size;
+  char *key;
+  char *value;
 };
 
-char *serialise(struct Command *command) {
-    char *serialised = kmalloc(sizeof(*command), GFP_KERNEL);
+char *serialise_command(struct command *command) {
+  char *serialised = kmalloc(sizeof(*command), GFP_KERNEL);
 
-    char *operation_ptr = serialised;
-    int *key_size_ptr = (int *) (serialised + sizeof(char));
-    int *value_size_ptr = key_size_ptr + sizeof(int);
-    char *key_ptr = (char *) (value_size_ptr + sizeof(char) * command->key_size);
-    char *value_ptr = key_ptr + sizeof(char) * command->value_size;
+  char *operation_ptr = serialised;
+  int *key_size_ptr = (int *) (serialised + sizeof(char));
+  int *value_size_ptr = key_size_ptr + sizeof(int);
+  char *key_ptr = (char *) (value_size_ptr + sizeof(char) * command->key_size);
+  char *value_ptr = key_ptr + sizeof(char) * command->value_size;
 
-    *operation_ptr = command->operation;
-    *key_size_ptr = command->key_size;
-    *value_size_ptr = command->value_size;
-    memcpy(key_ptr, command->key, command->key_size);
-    memcpy(value_ptr, command->value, command->value_size);
+  *operation_ptr = command->operation;
+  *key_size_ptr = command->key_size;
+  *value_size_ptr = command->value_size;
+  memcpy(key_ptr, command->key, command->key_size);
+  memcpy(value_ptr, command->value, command->value_size);
 
-    return serialised;
+  return serialised;
 };
 
-struct Command deserialise(char *data) {
-    char *operation_ptr = data;
-    int *key_size_ptr = (int *) (data + sizeof(char));
-    int *value_size_ptr = key_size_ptr + sizeof(int);
-    char *key_ptr = (char *) (value_size_ptr + sizeof(char) * (*key_size_ptr));
-    char *value_ptr = key_ptr + sizeof(char) * (*value_size_ptr);
+struct command deserialise_command(char *data) {
+  char *operation_ptr = data;
+  int *key_size_ptr = (int *) (data + sizeof(char));
+  int *value_size_ptr = key_size_ptr + sizeof(int);
+  char *key_ptr = (char *) (value_size_ptr + sizeof(char) * (*key_size_ptr));
+  char *value_ptr = key_ptr + sizeof(char) * (*value_size_ptr);
 
-    struct Command command;
-    command.operation = *operation_ptr;
-    command.key_size = *key_size_ptr;
-    command.value_size = *value_size_ptr;
-    command.key = kmalloc(sizeof(char) * (command.key_size - 1), GFP_KERNEL);
-    command.value = kmalloc(sizeof(char) * (command.value_size - 1), GFP_KERNEL);
-    memcpy(command.key, key_ptr, command.key_size);
-    memcpy(command.value, value_ptr, command.value_size);
-    return command;
+  struct command command;
+  command.operation = *operation_ptr;
+  command.key_size = *key_size_ptr;
+  command.value_size = *value_size_ptr;
+  command.key = kmalloc(sizeof(char) * (command.key_size - 1), GFP_KERNEL);
+  command.value = kmalloc(sizeof(char) * (command.value_size - 1), GFP_KERNEL);
+  memcpy(command.key, key_ptr, command.key_size);
+  memcpy(command.value, value_ptr, command.value_size);
+  return command;
 };
-
--- module/moddb.c
+++ module/moddb.c
@@ -9,14 +9,17 @@
 #include <linux/string.h>
 #include "command.c"
 
+// @formatter:off
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Shared key-value pair database.");
 MODULE_AUTHOR("Erik Ramos <c03ers@cs.umu.se>");
 MODULE_AUTHOR("Igor Ramazanov <ens17irv@cs.umu.se>");
 MODULE_AUTHOR("Harendarczyk Bastien<ens17bhk@cs.umu.se>");
 MODULE_VERSION("0.1");
+// @formatter:on
 
 static int test_hash(const char *, int, char *);
+
 static void print_hash(const char *);
 
 static const char *test_string = "Hello, World!";
@@ -24,72 +27,68 @@ static char hash_result[SHA256_DIGEST_SIZE];
 
 static int __init database_init(void)
 {
+  struct command command;
+  command.operation = 1;
+  char *key = "test_key";
+  char *value = "test_value";
+  command.key = key;
+  command.value = value;
+  command.key_size = strlen(key);
+  command.value_size = strlen(value);
 
+  char *serialized = serialise_command(&command);
+  struct command deserialised = deserialise_command(serialized);
+  printk(KERN_EMERG "Operation: %d", deserialised.operation);
+  printk(KERN_EMERG "Key size: %d", deserialised.key_size);
+  printk(KERN_EMERG "Key: %s", deserialised.key);
+  printk(KERN_EMERG "Value size: %d", deserialised.value_size);
+  printk(KERN_EMERG "Value: %s", deserialised.value);
 
-    struct Command command;
-    command.operation = 1;
-    char * key = "test_key";
-    char * value = "test_value";
-    command.key = key;
-    command.value = value;
-    command.key_size = strlen(key);
-    command.value_size = strlen(value);
+  test_hash(test_string, strlen(test_string), hash_result);
+  print_hash(hash_result);
 
-    char * serialized = serialise(&command);
-    struct Command deserialised = deserialise(serialized);
-    printk(KERN_EMERG "Operation: %d", deserialised.operation);
-    printk(KERN_EMERG "Key size: %d", deserialised.key_size);
-    printk(KERN_EMERG "Key: %s", deserialised.key);
-    printk(KERN_EMERG "Value size: %d", deserialised.value_size);
-    printk(KERN_EMERG "Value: %s", deserialised.value);
-
-	test_hash(test_string, strlen(test_string), hash_result);
-	print_hash(hash_result);
-
-    return 0;
+  return 0;
 }
 
 static void __exit database_exit(void)
 {
-    printk(KERN_DEBUG "moddb: module removed");
+  printk(KERN_DEBUG "moddb: module removed");
 }
 
-static int test_hash(const char *string, int length, char *result)
-{
-	struct shash_desc *desc = kmalloc(sizeof(struct shash_desc), GFP_KERNEL);
-	if (!desc) {
-		printk(KERN_ERR "moddb: Out of memory?");
-		return 1;
-	}
+static int test_hash(const char *string, int length, char *result) {
+  struct shash_desc *desc = kmalloc(sizeof(struct shash_desc), GFP_KERNEL);
+  if (!desc) {
+    printk(KERN_ERR "moddb: Out of memory?");
+    return 1;
+  }
 
-	desc->tfm = crypto_alloc_shash("sha256", CRYPTO_ALG_TYPE_SHASH, 0);
-	desc->flags = CRYPTO_TFM_REQ_MAY_SLEEP;
-	if (IS_ERR(desc->tfm)) {
-		printk(KERN_ERR "moddb: failed to allocate tfm");
-		kfree(desc);
-		return 1;
-	}
+  desc->tfm = crypto_alloc_shash("sha256", CRYPTO_ALG_TYPE_SHASH, 0);
+  desc->flags = CRYPTO_TFM_REQ_MAY_SLEEP;
+  if (IS_ERR(desc->tfm)) {
+    printk(KERN_ERR "moddb: failed to allocate tfm");
+    kfree(desc);
+    return 1;
+  }
 
-	memset(result, 0, SHA256_DIGEST_SIZE);
-	crypto_shash_init(desc);
-	crypto_shash_update(desc, string, length);
-	crypto_shash_final(desc, result);
+  memset(result, 0, SHA256_DIGEST_SIZE);
+  crypto_shash_init(desc);
+  crypto_shash_update(desc, string, length);
+  crypto_shash_final(desc, result);
 
-	crypto_free_shash(desc->tfm);
-	kfree(desc);
-	return 0;
+  crypto_free_shash(desc->tfm);
+  kfree(desc);
+  return 0;
 }
 
-static void print_hash(const char *hash)
-{
-	char string[2 * SHA256_DIGEST_SIZE + 1];
-	int i;
+static void print_hash(const char *hash) {
+  char string[2 * SHA256_DIGEST_SIZE + 1];
+  int i;
 
-	memset(string, 0, 2 * SHA256_DIGEST_SIZE + 1);
-	for (i = 0; i < SHA256_DIGEST_SIZE; i++)
-		snprintf(string + 2 * i, 2, "%02x", hash[i]);
+  memset(string, 0, 2 * SHA256_DIGEST_SIZE + 1);
+  for (i = 0; i < SHA256_DIGEST_SIZE; i++)
+    snprintf(string + 2 * i, 2, "%02x", hash[i]);
 
-	printk(KERN_EMERG "0x%s\n", string);
+  printk(KERN_EMERG "0x%s\n", string);
 }
 
 module_init(database_init);
