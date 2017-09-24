9a217179d1c55ac31c4c65cc4a21cb80b48d4c5d
Igor Ramazanov <themirrortruth@gmail.com>
2017-09-24 04:46:54 +0200

    Implemented sending commands from userspace to kernelspace

--- module/moddb.c
+++ module/moddb.c
@@ -5,9 +5,18 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <net/sock.h>
+#include <linux/skbuff.h>
+#include <linux/netlink.h>
 #include <linux/rhashtable.h>
 #include <linux/string.h>
+#include <linux/slab.h>
 #include "command.c"
+
+#ifndef NETLINK_USER
+#define NETLINK_USER 31
+#endif
+
 
 // @formatter:off
 MODULE_LICENSE("GPL");
@@ -25,24 +34,66 @@ static void print_hash(const char *);
 static const char *test_string = "Hello, World!";
 static char hash_result[SHA256_DIGEST_SIZE];
 
-static int __init database_init(void)
-{
+
+static struct sock *nlsk = NULL;
+
+static void proceed(char *payload, int len) {
+  struct command *a = kmalloc(len, GFP_KERNEL);
+  deserialise_command(a, payload);
+
+  printk(KERN_EMERG "Payload size: %d", len);
+  printk(KERN_EMERG "Operation: %d", a->operation);
+  printk(KERN_EMERG "Key size: %d", a->key_size);
+  printk(KERN_EMERG "Key: %s", a->key);
+  printk(KERN_EMERG "Value size: %d", a->value_size);
+  printk(KERN_EMERG "Value: %s", a->value);
+}
+
+static void nl_callback(struct sk_buff *skb) {
+  struct nlmsghdr *nlh;
+  int pid;
+  struct sk_buff *skb_out;
+  int msg_size;
+
   struct command command;
   command.operation = 1;
-  char *key = "test_key";
-  char *value = "test_value";
-  command.key = key;
-  command.value = value;
-  command.key_size = strlen(key);
-  command.value_size = strlen(value);
+  command.key = "key_from_kernel";
+  command.value = "value_from_kernel";
+  command.key_size = strlen(command.key) + 1;
+  command.value_size = strlen(command.value) + 1;
+  msg_size = sizeof(char) * (1 + command.value_size + command.key_size) + sizeof(int) * 2;
+  char *serialised = kmalloc(msg_size, GFP_KERNEL);
+  serialise_command(&command, serialised);
 
-  char *serialized = serialise_command(&command);
-  struct command deserialised = deserialise_command(serialized);
-  printk(KERN_EMERG "Operation: %d", deserialised.operation);
-  printk(KERN_EMERG "Key size: %d", deserialised.key_size);
-  printk(KERN_EMERG "Key: %s", deserialised.key);
-  printk(KERN_EMERG "Value size: %d", deserialised.value_size);
-  printk(KERN_EMERG "Value: %s", deserialised.value);
+  char *payload;
+  int len;
+
+  nlh = (struct nlmsghdr *) skb->data;
+  payload = (char *) nlmsg_data(nlh);
+  len = NLMSG_PAYLOAD(nlh, 0);
+  proceed(payload, len);
+  pid = nlh->nlmsg_pid; /*pid of sending process */
+
+  skb_out = nlmsg_new(msg_size, 0);
+
+  nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+  NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
+  strncpy(nlmsg_data(nlh), serialised, msg_size);
+
+  nlmsg_unicast(nlsk, skb_out, pid);
+}
+
+static int __init database_init(void)
+{
+  struct netlink_kernel_cfg cfg = {
+          .input = nl_callback,
+  };
+
+  nlsk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+  if (!nlsk) {
+    printk(KERN_ALERT "Error creating socket.\n");
+    return -10;
+  }
 
   test_hash(test_string, strlen(test_string), hash_result);
   print_hash(hash_result);
@@ -52,7 +103,7 @@ static int __init database_init(void)
 
 static void __exit database_exit(void)
 {
-  printk(KERN_DEBUG "moddb: module removed");
+  netlink_kernel_release(nlsk);
 }
 
 static int test_hash(const char *string, int length, char *result) {
--- program/main.c
+++ program/main.c
@@ -1,5 +1,134 @@
+#include <sys/socket.h>
 #include <linux/netlink.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-int main(int argc, char **argv) {
-  return 0;
+#define NETLINK_USER 31
+#define MAX_PAYLOAD 1024
+
+struct command {
+    //0 - Set, 1 - Get
+    char operation;
+    int key_size;
+    int value_size;
+    char *key;
+    char *value;
+};
+
+void serialise_command(struct command *command, char *data) {
+    *data = command->operation;
+    data++;
+
+    int *int_ptr = (int *) data;
+    *int_ptr = command->key_size;
+    int_ptr++;
+    *int_ptr = command->value_size;
+    int_ptr++;
+    char *char_ptr = (char *) int_ptr;
+    int i = 0;
+    while (i < command->key_size) {
+        *char_ptr = command->key[i];
+        char_ptr++;
+        i++;
+    }
+
+    i = 0;
+    while (i < command->value_size) {
+        *char_ptr = command->value[i];
+        char_ptr++;
+        i++;
+    }
+};
+
+void deserialise_command(struct command *command, char *data) {
+    command->operation = *data;
+    data++;
+    int *int_ptr = (int *) data;
+    command->key_size = *int_ptr;
+    int_ptr++;
+    command->value_size = *int_ptr;
+    int_ptr++;
+
+    command->key = malloc(sizeof(char) * (command->key_size));
+    command->value = malloc(sizeof(char) * (command->value_size));
+    char *char_ptr = (char *) int_ptr;
+    int i = 0;
+    while (i < command->key_size) {
+        command->key[i] = *char_ptr;
+        char_ptr++;
+        i++;
+    }
+    i = 0;
+    while (i < command->value_size) {
+        command->value[i] = *char_ptr;
+        char_ptr++;
+        i++;
+    }
+};
+
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
+
+int main() {
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+    if (sock_fd < 0)
+        return -1;
+
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = getpid(); /* self pid */
+
+    bind(sock_fd, (struct sockaddr *) &src_addr, sizeof(src_addr));
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0; /* For Linux Kernel */
+    dest_addr.nl_groups = 0; /* unicast */
+
+    struct command command;
+    command.operation = 1;
+    command.key = "test_key";
+    command.value = "test_value";
+    command.key_size = strlen(command.key) + 1;
+    command.value_size = strlen(command.value) + 1;
+    int command_size = sizeof(char) * (1 + command.value_size + command.key_size) + sizeof(int) * 2;
+    char *serialised = malloc(command_size);
+    serialise_command(&command, serialised);
+
+    printf("Command size: %d\n", command_size);
+
+    nlh = (struct nlmsghdr *) malloc(NLMSG_SPACE(command_size));
+    memset(nlh, 0, NLMSG_SPACE(command_size));
+    nlh->nlmsg_len = NLMSG_SPACE(command_size);
+    nlh->nlmsg_pid = getpid();
+    nlh->nlmsg_flags = 0;
+
+    memcpy(NLMSG_DATA(nlh), serialised, command_size);
+
+    iov.iov_base = (void *) nlh;
+    iov.iov_len = nlh->nlmsg_len;
+    msg.msg_name = (void *) &dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    printf("Sending message to kernel\n");
+    sendmsg(sock_fd, &msg, 0);
+    printf("Waiting for message from kernel\n");
+
+/* Read message from kernel */
+    recvmsg(sock_fd, &msg, 0);
+    int len = NLMSG_PAYLOAD(nlh, 0);
+    char * buff = malloc(len);
+    struct command* cmd;
+    deserialise_command(cmd, buff);
+
+    printf("%s\n", cmd->key);
+    printf("%s\n", cmd->value);
+    close(sock_fd);
 }
